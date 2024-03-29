b19c79c42d4e7d1b55dd491720f532b918f1f771
AsifuzamanAsif <asifuzzamanasif28@gmail.com>
2024-03-29 23:42:39 +0600

    User

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function User() {
-  return <div>User</div>;
+  return (
+    <div className="py-96 px-64 w-16">
+      <div className="card" />
+    </div>
+  );
 }
 
 export default User;
