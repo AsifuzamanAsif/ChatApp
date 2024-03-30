eceaaf5d0230e33dc695da3499c3f2c27c2266da
AsifuzamanAsif <asifuzzamanasif28@gmail.com>
2024-03-30 23:52:25 +0600

    User

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -2,8 +2,31 @@ import React from "react";
 
 function User() {
   return (
-    <div className="py-96 px-64 w-16">
-      <div className="card" />
+    <div className="wrap font-primary">
+      <div className="profile ">
+        <img
+          src="https://images.unsplash.com/photo-1687462310643-e993a3f09102?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb3RhaXQlMjBpbWd8ZW58MHx8MHx8fDA%3D"
+          className="photo w-[80px] h-[80px]"
+        />
+        <div className="pt-3 ">
+          <span className="profile_name font-primary">Kelvin Queiróz</span>
+          <span className="at text-white">@_kelvinqueiroz</span>
+        </div>
+      </div>
+      <div className="links">
+        <ul>
+          <li>
+            <a href="#" className="link">
+              <i className="fab fa-youtube" /> See the latest video
+            </a>
+          </li>
+          <li>
+            <a href="#" className="link">
+              <i className="fab fa-youtube" /> Subscribe on YouTube channel
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
