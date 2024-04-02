b447b9be061fa0c00f47ca9456a4e742fe1cfdcc
AsifuzamanAsif <asifuzzamanasif28@gmail.com>
2024-04-02 22:58:29 +0600

    user

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function User() {
   return (
-    <div className="wrap font-primary">
+    <div className="wrap font-primary bg-slate-600">
       <div className="profile ">
         <img
           src="https://images.unsplash.com/photo-1687462310643-e993a3f09102?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb3RhaXQlMjBpbWd8ZW58MHx8MHx8fDA%3D"
